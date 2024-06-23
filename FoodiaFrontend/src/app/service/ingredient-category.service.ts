001b18d95f5845af02db1d917d1e519a13ef062d
Lizzie6666 <lizziehernandezvillanueva@gmail.com>
2024-06-23 18:50:33 -0500

    Ingredient, Ingredient-Category
    Model y service

---
+++ FoodiaFrontend/src/app/service/ingredient-category.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { IngredientCategory } from '../model/ingredient-category';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+import { enviroment } from '../../environments/environment';
+const base_url = enviroment.base
+@Injectable({
+  providedIn: 'root'
+})
+export class IngredientCategoryService {
+
+  private url = `${base_url}` //OJOx
+  private lista = new Subject<IngredientCategory[]>();
+  constructor(private http:HttpClient) { }
+
+  list() : Observable<any>{
+    return this.http.get<IngredientCategory[]> (this.url+"/ingredientCategory/list");
+  }
+
+  insert(recipe:IngredientCategory){
+    return this.http.post(this.url+ "/ingredientCategory/save", recipe);
+  }
+  update(rec: IngredientCategory){
+    return this.http.put(this.url + "/ingredientCategory/update", rec);
+  }
+  delete(id:number){
+    return this.http.delete(this.url + "/ingredientCategory/delete/" + id);
+  }
+  setList(listaNueva : IngredientCategory[]){
+    this.lista.next(listaNueva);//enviar la nueva lista a los suscriptores
+  }
+  getList(){
+    return this.lista.asObservable();
+  }
+}
