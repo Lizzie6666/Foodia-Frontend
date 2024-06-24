457af6b934b13f38c2cfa199fd060d1a96cf30f9
Lizzie6666 <lizziehernandezvillanueva@gmail.com>
2024-06-23 19:33:18 -0500

    Ingredient component

--- FoodiaFrontend/src/app/service/ingredient.service.ts
+++ FoodiaFrontend/src/app/service/ingredient.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
-
+import { Ingredient } from '../model/ingredient';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable, Subject } from 'rxjs';
+const base_url = environment.base
 @Injectable({
   providedIn: 'root'
 })
 export class IngredientService {
 
-  constructor() { }
+  private url = `${base_url}` //OJOx
+  private lista = new Subject<Ingredient[]>();
+  constructor(private http:HttpClient) { }
+
+  list() : Observable<any>{
+    return this.http.get<Ingredient[]> (this.url+"/ingredient/list");
+  }
+  findByName(name:string){
+    return this.http.get(this.url+"/ingredient/findByName/"+name)
+  }
+  listById(id:number) : Observable<any>{
+    console.log("ListId:"+this.url+"/ingredient/listById/"+id);
+    return this.http.get<Ingredient> (this.url+"/ingredient/listById/"+id);
+  }
+  insert(recipe:Ingredient){
+    return this.http.post(this.url+ "/ingredient/save", recipe);
+  }
+  update(rec: Ingredient){
+    return this.http.put(this.url + "/ingredient/update", rec);
+  }
+  delete(id:number){
+    return this.http.delete(this.url + "/ingredient/delete/" + id);
+  }
+  setList(listaNueva : Ingredient[]){
+    this.lista.next(listaNueva);//enviar la nueva lista a los suscriptores
+  }
+  getList(){
+    return this.lista.asObservable();
+  }
 }
